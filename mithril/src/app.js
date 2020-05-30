b27fbede198d908e0cbbf81b06dad002d6c9d635
resolritter <joao.paulo.silvasouza@hotmail.com>
2020-05-30 00:26:23 -0300

    update mithril example to better showcase 'useState'

--- mithril/src/app.js
+++ mithril/src/app.js
@@ -1,5 +1,12 @@
 import m from "mithril"
-import { useHooks, newHookKey } from "../../src/hooks.js"
+import { useHooks, newHookKey, newHook } from "../../src/hooks.js"
+
+const useRenderCounter = newHook(function (h) {
+  const { get, set } = h.useRef(0)
+  const newValue = get() + 1
+  set(newValue)
+  return newValue
+})
 
 function C() {
   const id = newHookKey()
@@ -26,40 +33,44 @@ function C() {
 }
 
 function Tock() {
+  let h, renderCount
   return {
-    view({ attrs: { value } }) {
-      return m("span", `Tock ${value}`)
+    view(v) {
+      h = useHooks(v.attrs.id)
+      renderCount = useRenderCounter(h)
+      return m("span", `Tock ${v.attrs.value} has been rendered ${renderCount} times.`)
     },
   }
 }
 
 function Tick() {
-  let h, state
-  function getValue(v) {
+  let h, state, renderCounter
+  // hack for getting the latest value, otherwise `render` would capture an outdated value
+  function getLastVNodeValue(v) {
     return window[v.attrs.id]
   }
-  function tree(value) {
+  function render(value) {
+    const renderCount = renderCounter.get() + 1
+    renderCounter.set(renderCount)
     return [
-      m("span", `Tick ${value}`),
-      m("span", `Sibling: ${state.get()}`),
+      m("span", `Tick ${value} has been rendered ${renderCount} times. `),
+      m("span", `useState value: ${state.get()} `),
       m("button", {
-        onclick: function (e) {
-          e.redraw = false
+        onclick: function () {
           state.set(state.get() + 1)
         },
-      }, "Increment sibling"),
+      }, "Increment useState (triggers render)"),
     ]
   }
   return {
-    oninit(v) {
-      h = useHooks(v.attrs.id)
-      state = h.useState(0, function () {
-        m.render(v.dom, tree(getValue(v)))
-      })
-    },
     view(v) {
       window[v.attrs.id] = v.attrs.value
-      return m("div", tree(v.attrs.value))
+      h = useHooks(v.attrs.id)
+      state = h.useState(0, function () {
+        m.render(v.dom, render(getLastVNodeValue(v)))
+      })
+      renderCounter = h.useRef(0)
+      return m("div", render(v.attrs.value))
     },
   }
 }
@@ -69,6 +80,9 @@ export default function () {
   let tickTockInterval, h, tick, tock
   return {
     oninit() {
+      // parent can declare hooks only once because it's not using the values in
+      // the view; otherwise, those hooks would need to be moved to the view
+      // method
       h = useHooks("app")
       tick = h.useContext("tick", 0)
       tock = h.useContext("tock", 0)
@@ -86,8 +100,8 @@ export default function () {
     },
     view() {
       return m("div", [
-        m(C, { ...tick.childProps(), C: Tick }),
-        m(C, { ...tock.childProps(), C: Tock }),
+        m(C, { C: Tick, ...tick.childProps() }),
+        m(C, { C: Tock, ...tock.childProps() }),
       ])
     },
   }
